93ea1abfbcd6eb9b21e435926ab2a7e975e47f12
Nikita Kirdiapin <nikita.kirdiapin@gmail.com>
2020-03-05 11:22:27 +0300

    add animation

--- src/components/Content/SaveCityList.js
+++ src/components/Content/SaveCityList.js
@@ -1,39 +1,39 @@
 import React from "react";
 
-// import { CSSTransition, TransitionGroup } from "react-transition-group";
-const CitiesList = props => {
-	const articleElements = props.listSaveCity.map((city, index) => (
-		// <TransitionGroup>
-		// 	<CSSTransition classNames="option" key={index} timeout={500}>
-		<li
-			key={index}
-			index={index}
-			className="sities__item"
-			onClick={e => {
-				props.select_city(e, city.city);
-			}}
-		>
-			<div className="card">
-				<div className="card__header">
-					{city.city}, {city.country}
-				</div>
-				<div className="card__body">
-					Temp today {Math.round(city.temp)} {props.degrees_icon}
-				</div>
-				{/* <button className="button" onClick={() => props.remove_city(index)}>
-						delete
-					</button> */}
-			</div>
-		</li>
-		// 	</CSSTransition>
-		// </TransitionGroup>
-	));
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+
+function CitiesList(props) {
 	return (
 		<div className="wrapper">
 			<h3 className="section__title">Saved cities</h3>
-			<ul className="cities__list">{articleElements}</ul>
+			<ul className="cities__list">
+				<TransitionGroup className="cities__list" defaultValue="dsd">
+					{props.listSaveCity.map((city, index) => (
+						<CSSTransition key={index} timeout={500} classNames="option">
+							<li
+								className="sities__item"
+								onClick={e => {
+									props.select_city(e, city.city);
+								}}
+							>
+								<div className="card">
+									<div className="card__header">
+										{city.city}, {city.country}
+									</div>
+									<div className="card__body">
+										Temp today {Math.round(city.temp)} {props.degrees_icon}
+									</div>
+									{/* <button className="button" onClick={() => props.remove_city(index)}>
+						delete
+					</button> */}
+								</div>
+							</li>
+						</CSSTransition>
+					))}
+				</TransitionGroup>
+			</ul>
 		</div>
 	);
-};
+}
 
 export default CitiesList;
